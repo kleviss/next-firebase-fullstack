38f44141ea589db96384094b33d610b36d7adec6
Klevis macXhyra <klevis.xhyra@mova.ie>
2023-04-27 12:34:22 +0200

    add tailwind and style free lessons page

--- src/app/free-lessons/page.js
+++ src/app/free-lessons/page.js
@@ -1,24 +1,45 @@
 "use client";
 import React from "react";
-import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import styles from "../../app/page.module.css";
 import FreeLessons from "@/components/FreeLessons";
 import Link from "next/link";
+import { Popover } from '@headlessui/react'
+
 
 const FreeLessonsPage = ({ lessons }) => {
   const router = useRouter();
 
   return (
-    <main className={styles.main}>
-      <>
-        <div>
-          <h1>Mesime pa lek</h1>
-          <Link href="/"> Faqja Kryesore </Link>
+      <main className="bg-gray-100 min-h-screen">
+        <nav className="bg-white shadow-lg fixed top-0 w-full z-10">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex">
+                <div className="flex-shrink-0 flex items-center">
+                  <img className="h-8 w-8" src="/Vfavicon-150x150.jpg" alt="Logo" />
+                  <span className="font-medium text-black px-3 py-2 rounded-md text-sm">VISION FX</span>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="hidden md:block">
+                  <Link className="font-medium text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm" href={"/"}>
+                    Faqja Kryesore
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+
+
+
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-6">
+          <FreeLessons lessons={lessons} />
         </div>
-        <FreeLessons lessons={lessons} />
-      </>
-    </main>
+
+      </main>
   );
 };
 
--- src/components/FreeLessons.js
+++ src/components/FreeLessons.js
@@ -1,5 +1,5 @@
-"use client";
 import React from "react";
+import LessonCard from "./LessonCard";
 
 const FreeLessons = () => {
   const [lessons, setLessons] = React.useState([
@@ -7,35 +7,76 @@ const FreeLessons = () => {
       id: 1,
       title: "Bej lek pa lek",
       videoUrl: "https://www.youtube.com/watch?v=7CqJlxBYj-M",
+      imageUrl: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      duration: "1:30",
+      views: 100,
+      createdOn: "2020-10-10",
+      authorImage: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      category: "FOREX",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl vitae tincidunt ultricies, nunc nisl ultricies nunc, vitae luctus",
     },
     {
       id: 2,
       title: "Menaxho rrezikun o cun",
       videoUrl: "https://www.youtube.com/watch?v=xPZoHsaRVzM",
+      imageUrl: "https://i.ytimg.com/vi/mxK8b99iJTg/maxresdefault.jpg",
+      duration: "5:30",
+      views: 200,
+      createdOn: "2020-10-10",
+      authorImage: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      category: "FOREX",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl vitae tincidunt ultricies, nunc nisl ultricies nunc, vitae luctus",
+
     },
     {
       id: 3,
-      title: "fibonaci, esht me lek, mvje keq ;)",
+      title: "Fibonaci, esht me lek, mvje keq ;)",
       videoUrl: "https://www.youtube.com/watch?v=mxK8b99iJTg",
+      imageUrl: "https://i.ytimg.com/vi/mxK8b99iJTg/maxresdefault.jpg",
+      duration: "6:30",
+      views: 300,
+      createdOn: "2020-10-10",
+      authorImage: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      category: "CRYPTO",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl vitae tincidunt ultricies, nunc nisl ultricies nunc, vitae luctus",
     },
+    {
+      id: 2,
+      title: "Menaxho rrezikun o cun",
+      videoUrl: "https://www.youtube.com/watch?v=xPZoHsaRVzM",
+      imageUrl: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      duration: "5:30",
+      views: 200,
+      createdOn: "2020-10-10",
+      authorImage: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      category: "FOREX",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl vitae tincidunt ultricies, nunc nisl ultricies nunc, vitae luctus",
+
+    },
+    {
+      id: 3,
+      title: "Fibonaci, esht me lek, mvje keq ;)",
+      videoUrl: "https://www.youtube.com/watch?v=mxK8b99iJTg",
+      imageUrl: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      duration: "6:30",
+      views: 300,
+      createdOn: "2020-10-10",
+      authorImage: "https://i.ytimg.com/vi/7CqJlxBYj-M/maxresdefault.jpg",
+      category: "CRYPTO",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl vitae tincidunt ultricies, nunc nisl ultricies nunc, vitae luctus",
+    },
+
   ]);
 
   return (
-    <div>
-      {lessons.map((lesson) => (
-        <div key={lesson.id}>
-          <h2>{lesson.title}</h2>
-          <iframe
-            src={lesson.videoUrl}
-            title={lesson.title}
-            width="560"
-            height="315"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </div>
-      ))}
-    </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-20 grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <h1 className="text-3xl font-bold text-gray-900 col-span-2">Mesime Pa Lek</h1>
+        {lessons.map((lesson) => (
+            <LessonCard key={lesson.id} lesson={lesson} />
+        ))}
+      </div>
+
+
   );
 };
 
